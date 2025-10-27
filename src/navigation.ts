69b1cc3bcfefe9e342d226498f24b4c6976bcb5f
vfermi <30763171+valfermi@users.noreply.github.com>
2025-10-27 03:09:51 +0000

    feat: add 2025 christmas tree campaign link

--- src/navigation.ts
+++ src/navigation.ts
@@ -19,7 +19,7 @@ export const headerData = {
     //   href: getPermalink('/docs'),
     // },
     {
-      text: 'Vente de café!',
+      text: 'Vente de sapins!',
       href: getPermalink('/boutique'),
     },
 
