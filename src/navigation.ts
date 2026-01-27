3486f9718de024cfcfd978fcf5c51072eed265cf
vfermi <30763171+valfermi@users.noreply.github.com>
2026-01-27 04:18:21 +0000

    feat: add feb 2026 coffee campaign link

--- src/navigation.ts
+++ src/navigation.ts
@@ -19,7 +19,7 @@ export const headerData = {
     //   href: getPermalink('/docs'),
     // },
     {
-      text: 'Vente de sapins!',
+      text: 'Vente de café',
       href: getPermalink('/boutique'),
     },
 
