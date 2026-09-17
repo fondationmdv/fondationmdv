3438af9a56d0e7bfbb2f12597331d5b44d5b2886
valerian.fermi <valerian.fermi@expretio.com>
2026-09-16 22:53:36 -0400

    Update boutique.astro - aucun lien

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,4 +1,4 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink } from './utils/permalinks';
 
 export const headerData = {
   links: [
@@ -10,10 +10,10 @@ export const headerData = {
       text: 'Don',
       href: getPermalink('/don'),
     },
-    {
-      text: 'Vente de sapins!',
-      href: getPermalink('/boutique'),
-    },
+    // {
+    //   text: 'Vente de sapins!',
+    //   href: getPermalink('/boutique'),
+    // },
 
     // {
     //   text: 'A propos',
